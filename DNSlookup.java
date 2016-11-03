4e09aaca88984fb7ce1b402e632706d6bec9c3af
goldenied <markrepedersen@gmail.com>
2016-11-02 18:31:58 -0700

    Added functionality for encoding query

--- DNSlookup.java
+++ DNSlookup.java
@@ -2,6 +2,7 @@
  * Created by mark on 2016-11-01.
  */
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -15,8 +16,7 @@ import java.util.Random;
 
 /**
  * @author Donald Acton
- * This example is adapted from Kurose & Ross
- *
+ *         This example is adapted from Kurose & Ross
  */
 public class DNSlookup {
     
@@ -31,28 +31,59 @@ public class DNSlookup {
     // - port number is 53 by default
     // user of this method must handle both exceptions: SocketException, IOException
     // returns the server's response
-    public static void sendQuery(InetAddress address, String fqdn) throws SocketException, IOException {
+    public static void sendQuery(DatagramSocket socket, InetAddress address, String fqdn) throws SocketException, IOException {
         // SENDIN' DA PACKAGE..
-        DatagramSocket socket = new DatagramSocket();
-        byte[] ip = fqdn.getBytes();
+        byte[] ip = convertDomainNameToDNSQuery(fqdn);
         DatagramPacket packet = new DatagramPacket(ip, ip.length, address, 53);
         socket.send(packet);
-        
+    }
+    
+    public static void receiveQuery(DatagramSocket socket) throws IOException {
         // RECEIVIN' DA PACKAGE..
         // **** if no response within 5 secs, resend packet *****
         // ******** if still no response, throw exception/print error code
         // *********** MAKE SURE TO CHECK THAT RECEIVED PACKET IS NOT TRUNCATED *********
         // *********** IF TRUNCATED (IF PAYLOAD WAS TOO LARGE FOR PACKET) THROW SOME ERROR *********
         byte[] buf = new byte[1023];
-        packet = new DatagramPacket(buf, buf.length);
-        boolean isGoodTransactionID = false;
-        while (isGoodTransactionID) {
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        boolean transactionSuccess = false;
+        while (transactionSuccess) {
             // if transaction id from response is different from query, discard response and
             // start receiving again
             socket.receive(packet);
+            
+        }
+    }
+    
+    public static byte[] convertDomainNameToDNSQuery(String fqdn) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        // Creates a random queryID between 0 - (2^16 - 1) inclusive
+        Random r = new Random();
+        byte queryID = (byte) r.nextInt(65535);
+        byte[] prefix = {queryID, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+        outputStream.write(prefix);
+        convertToByteArray(outputStream, fqdn.split("."));
+        byte[] postfix = {(byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x01};
+        outputStream.write(postfix);
+        return outputStream.toByteArray();
+    }
+    
+    private static byte[] convertToByteArray(ByteArrayOutputStream outputStream, String[] strings) throws IOException {
+        byte[] data = new byte[strings.length];
+        
+        String[] dnsQueryString = new String[strings.length * 2];
+        for (int i = 0, j = 0; i < strings.length; i++, j = j + 2) {
+            String string = strings[i];
+            dnsQueryString[j] = String.valueOf(string.length());
+            dnsQueryString[j + 1] = string;
         }
         
-        
+        for (int i = 0; i < dnsQueryString.length; i++) {
+            String string = dnsQueryString[i];
+            outputStream.write(string.getBytes());
+        }
+        return outputStream.toByteArray();
     }
     
     /**
@@ -75,13 +106,14 @@ public class DNSlookup {
             tracingOn = true;
         
         // Start adding code here to initiate the lookup
-        try {
-            sendQuery(rootNameServer, fqdn);
-        }
-        catch (SocketException e) {
+        try (
+             DatagramSocket socket = new DatagramSocket();
+             ) {
+            sendQuery(socket, rootNameServer, fqdn);
+            receiveQuery(socket);
+        } catch (SocketException e) {
             //do something
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             //do something
         }
         
